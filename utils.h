9b87f17979adc3dfeb5e2f836c5be506f64b6b1d
Luiz Felipe <felipe.maciel@lsbd.ufc.br>
2019-04-05 09:11:22 -0300

    Added constants in utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define INDEX   0
+#define POINTER 1
+
+#define LAST    0
+#define MID     1
+#define MD5     2
+
+
 void swap(int *a, int *b);
 int* generateIntArray(int n, int min, int max);
 void printArray(int *arr, int n);
